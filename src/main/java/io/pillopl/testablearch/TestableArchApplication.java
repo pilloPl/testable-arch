1e0c072509a09da3c583a91b31a54de7f4516922
Jakub Pilimon <jpilimon@jpilimon-a01.vmware.com>
2020-07-26 15:49:16 +0200

    added maven support

---
+++ src/main/java/io/pillopl/testablearch/TestableArchApplication.java
@@ -0,0 +1,13 @@
+package io.pillopl.testablearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestableArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestableArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pillopl/testablearch/TestableArchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pillopl.testablearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestableArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
