22b0e5265991bb024a12ce5eb25957f7eb61980f
Jakub Pilimon <jpilimon@jpilimon-a01.vmware.com>
2020-07-28 19:42:29 +0200

    arch unit

---
+++ src/test/java/io/pillopl/testablearch/ex2/bigballofmud/TestOfArchitecture.java
@@ -0,0 +1,15 @@
+package io.pillopl.testablearch.ex2.bigballofmud;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.runner.RunWith;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+@AnalyzeClasses(packages = "io.pillopl.testablearch.ex2")
+class TestOfArchitecture {
+
+
+
+}
