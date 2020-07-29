f87f3c59274f027ce8a9c4c778e1b61a9464f886
Jakub Pilimon <jpilimon@jpilimon-a01.vmware.com>
2020-07-29 11:16:15 +0200

    ex2 done

--- src/test/java/io/pillopl/testablearch/ex2/acl/SanityFeatureToggleTest.java
+++ src/test/java/io/pillopl/testablearch/ex2/acl/SanityFeatureToggleTest.java
@@ -2,12 +2,14 @@ package io.pillopl.testablearch.ex2.acl;
 
 import io.pillopl.testablearch.ex2.acl.toggles.NewModelToggles;
 import io.pillopl.testablearch.ex2.bigballofmud.BigBallOfMud;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.togglz.core.manager.FeatureManager;
+import org.togglz.junit.TogglzRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,9 +20,15 @@ public class SanityFeatureToggleTest {
     @Autowired
     FeatureManager manager;
 
+    @Rule
+    TogglzRule togglzRule = TogglzRule.allDisabled(NewModelToggles.class);
+
     @Test
     public void makeSureNewModelIsDisabledOnProd() {
- 
+
+        togglzRule.disable(NewModelToggles.RECONCILE_NEW_MODEL);
+
+        assertThat(manager.getFeatureState(NewModelToggles.RECONCILE_AND_USE_NEW_MODEL).isEnabled()).isFalse();
     }
 
 }
